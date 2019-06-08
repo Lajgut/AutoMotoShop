9bfb5ea4970bdb2a6aa9f049c1a8504286881efe
bit <bitkovkir@gmail.com>
2019-06-08 23:48:12 +0300

    first test

--- lib/widget/ContactUsWidget.dart
+++ lib/widget/ContactUsWidget.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 
-
-
-
-
 class ContactUsWidget extends StatefulWidget {
-
   @override
   State createState() => _ContactUsState();
-
-
 }
 
 class _ContactUsState extends State<ContactUsWidget> {
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) => Container(
+      decoration: new BoxDecoration(
+        image: new DecorationImage(image: AssetImage("assets/images/bg.jpg")),
+      ),
+      //color: Colors.indigoAccent,
+      child: Container(
+        color: Color.fromARGB(220, 80, 80, 80),
+        child: Column(children: <Widget>[
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+                padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+                child: Text("89997773344", style: _getFontStyle())),
+          ),
+          Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                  padding: EdgeInsets.fromLTRB(16, 8, 16, 0),
+                  child:
+                      Text("mymaillongmail@mail.com", style: _getFontStyle())))
+        ]),
+      ));
 
-  }
+  Widget _buildContactUsForm() => Container(
+
+  );
+
+  TextStyle _getFontStyle() => TextStyle(color: Colors.white, fontSize: 24);
 }
