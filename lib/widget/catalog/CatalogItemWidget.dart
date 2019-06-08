22e9cab6ce67689ba72f0cb242bc95629b1a0c07
bit <bitkovkir@gmail.com>
2019-06-08 21:25:54 +0300

    done page view

--- lib/widget/catalog/CatalogItemWidget.dart
+++ lib/widget/catalog/CatalogItemWidget.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 
 class CatalogItemWidget extends StatefulWidget {
-
   @override
   State createState() => _CatalogItemState();
 }
 
 class _CatalogItemState extends State<CatalogItemWidget> {
-
-
   @override
   Widget build(BuildContext context) => Container(
-    child: Image.asset("assets/images/pic"),
-  );
+        child: Image(
+            image: AssetImage("assets/images/pic.jpeg")),
+      );
 }
--- lib/widget/catalog/CatalogWidget.dart
+++ lib/widget/catalog/CatalogWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:auto_moto_shop/widget/catalog/CatalogItemWidget.dart';
 
 class CatalogWidget extends StatefulWidget {
   @override
@@ -9,18 +10,27 @@ class _CatalogState extends State<CatalogWidget> {
   @override
   Widget build(BuildContext context) => Container(
           child: Column(children: [
-        Text(
+        /*Text(
           "каталог какой то",
           style: TextStyle(color: Colors.black, fontSize: 21),
           textAlign: TextAlign.center,
-        ),
-        PageView(
-          controller: _buildPageController(),
-          children: <Widget>[
-
-          ],
+        ),*/
+        SizedBox(
+          height: 200,
+          child: PageView(
+            controller: _buildPageController(),
+            children: _createPageViewItems()
+          ),
         )
       ]));
 
   PageController _buildPageController() => PageController(initialPage: 1);
+
+  List<CatalogItemWidget> _createPageViewItems() {
+    var widgets = new List<CatalogItemWidget>();
+    for (var i = 0; i < 10; i++) {
+      widgets.add(CatalogItemWidget());
+    }
+    return widgets;
+  }
 }
